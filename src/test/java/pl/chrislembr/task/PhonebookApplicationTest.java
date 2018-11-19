e1010159ba6c929836d12b719df450d4c1533495
Christian Lembrandt <christianlembrandt@gmail.com>
2018-11-19 11:33:47 +0100

    update 19.11.18 // 12:00

---
+++ src/test/java/pl/chrislembr/task/PhonebookApplicationTest.java
@@ -0,0 +1,7 @@
+package pl.chrislembr.task;
+
+import static org.junit.Assert.*;
+
+public class PhonebookApplicationTest {
+
+}
---
+++ src/test/java/pl/chrislembr/task/TaskStartTest.java
@@ -0,0 +1,7 @@
+package pl.chrislembr.task;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskStartTest {
+
+}
